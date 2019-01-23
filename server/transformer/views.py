9981e8d9760db4feb57c39fe9a1abebc8603a11c
Men Rui <809505910@qq.com>
2019-01-23 20:57:15 +0800

    修改格式

--- server/transformer/views.py
+++ server/transformer/views.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse, HttpResponse
 from .sampler import transform_sampler
-
+import json
 
 def hello(request):
     return HttpResponse("Hello world ! ")
 
 
 def trans(request):
-    timestamp = request.POST['timestamp']
-    image = request.POST['image']
+    data = json.loads(request.body)
+    timestamp = data['timestamp']
+    image = data['image']
     file_name = transform_sampler.transform(image)
     return JsonResponse({'timestamp': timestamp, 'file_name': file_name})
