9b5d0479ed7bdf3e0e2b5f52b97625229d13f681
Men Rui <809505910@qq.com>
2019-01-23 20:32:03 +0800

    bug fixed

--- server/server/urls.py
+++ server/server/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-
-from . import view
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^$', view.hello),
-    url(r'^trans/$', view.trans)
+    path('transformer/', include('transformer.urls')),
 ]
