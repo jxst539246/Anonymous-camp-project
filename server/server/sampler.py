9c7d6896e5716bc9b54da69c42dc0fac544acb44
Men Rui <809505910@qq.com>
2019-01-23 15:49:16 +0800

    init django project

---
+++ server/server/sampler.py
@@ -0,0 +1,46 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from PIL import Image
+from .models import GeneratorResNet
+import re
+import base64
+from io import BytesIO
+from torch.autograd import Variable
+import uuid
+
+
+def base64_to_image(base64_str):
+    base64_data = re.sub('^data:image/.+;base64,', '', base64_str)
+    byte_data = base64.b64decode(base64_data)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    return img
+
+
+class Sampler():
+    def __init__(self, use_cuda=True):
+        self.G = GeneratorResNet()
+        if use_cuda:
+            self.G = self.G.cuda()
+        transforms_ = [transforms.Resize((256, 256)),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        self.transformer = transforms.Compose(transforms_)
+        self.Tensor = torch.cuda.FloatTensor if use_cuda else torch.Tensor
+
+    def load_check_point(self, check_point_path):
+        self.G.load_state_dict(torch.load(check_point_path))
+
+    def transform(self, image_str):
+        self.load_check_point('./final_models/G.th')
+        img = base64_to_image(image_str)
+        img = self.transformer(img)
+        real_A = Variable(img.reshape(1, 3, 256, 256).type(self.Tensor))
+        img_sample = self.G(real_A)
+        file_name = 'output/' + uuid.uuid4() + '.png'
+        save_image(img_sample, file_name, normalize=True)
+        return file_name
+
+
+transform_sampler = Sampler(use_cuda=False)
---
+++ server/server/view.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse, HttpResponse
+from .sampler import transform_sampler
+
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+
+def trans(request):
+    timestamp = request.POST['timestamp']
+    image = request.POST['image']
+    file_name = transform_sampler(image)
+    return JsonResponse({'timestamp': timestamp, 'file_name': file_name})
